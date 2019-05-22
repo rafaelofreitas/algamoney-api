b89e7c8c28e7b333617896e782ac9ccfc940e987
Rafael de Oliveira Freitas <rafa.oliveirafreitas@hotmail.com>
2019-05-22 18:46:16 -0300

    regra para nao salvar pessoa inativa ou inexistente

--- src/main/java/com/algaworks/algamoney/api/model/Pessoa.java
+++ src/main/java/com/algaworks/algamoney/api/model/Pessoa.java
@@ -1,5 +1,7 @@
 package com.algaworks.algamoney.api.model;
 
+import java.beans.Transient;
+
 import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -7,6 +9,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "pessoa")
@@ -56,7 +60,13 @@ public class Pessoa {
 	public void setAtivo(Boolean ativo) {
 		this.ativo = ativo;
 	}
-
+	
+	@JsonIgnore
+	@Transient
+	public boolean isInativo() {
+		return !this.ativo;
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
